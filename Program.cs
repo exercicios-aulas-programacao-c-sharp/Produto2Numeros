6466b627b6125edaf35726c6d0130d431ccc8264
diego_neri <diego@diegoneri.com>
2020-05-20 20:59:32 -0300

    Exercício exemplo - Produto de dois números

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Produto2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entradaNumeroUm;
+            string entradaNumeroDois;
+            
+            double numeroUm;
+            double numeroDois;
+
+            double produto;
+
+            Console.WriteLine("---Produto de dois números---");
+            
+            Console.Write("Digite o primeiro número:\t");
+            entradaNumeroUm = Console.ReadLine();
+            
+            Console.Write("Digite o segundo número:\t");
+            entradaNumeroDois = Console.ReadLine();
+
+            numeroUm = Convert.ToDouble(entradaNumeroUm);
+            numeroDois = Convert.ToDouble(entradaNumeroDois);
+
+            produto = numeroUm * numeroDois;
+
+            Console.WriteLine($"O produto de {numeroUm} e {numeroDois} é {produto}");
+
+        }
+    }
+}
